1fa2c3610638079bd9eb7357cfd307a6cc8d7051
jeyaprakash1 <jpofficial1232@gmail.com>
2021-04-17 08:52:13 +0530

    successfully updated

---
+++ list_test.py
@@ -0,0 +1,79 @@
+# append function to add element
+
+l=[10,20,30,10]
+l2=l.index(30)
+print(l2)
+
+# insert function to insert a element
+
+l1=['a','b','c']
+l1.insert(3,'d')
+print(l1)
+
+# Extend one list to another list
+
+s1=["one","two","three"]
+print(len(s1))
+s2=[4,5]
+s1.extend(s2)
+print(s1)
+print(len(s1))
+
+# Remove function to remove particular element
+
+sen=['sun','mon','fri']
+sen.remove('fri')
+print(sen)
+
+# pop function to remove elements in last position and particular position
+
+list1=['prakash','gandhi','muthu','vignesh']
+list1.pop()
+print(list1)
+list1.pop(2)
+print(list1)
+
+# Reverse function to reverse a list
+
+l1=['a','b','c','d']
+l1.reverse()
+print(l1)
+
+# Split a string and its stored in list
+
+l= " Wish you happy birthday "
+grp=l.split()
+print(grp)
+
+# join function
+
+sen=["hi","hello"]
+sen=' '.join(sen)
+print(sen)
+
+l=[10,20,30,40,50,10,70,80,10]
+l2=l.index(10,2,9)
+print(l2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
